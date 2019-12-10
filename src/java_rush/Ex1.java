50dd98f1fb725c8605da8e73f6dceb4b60138131
DavydovNix <D30011921d>
2019-12-10 08:35:20 +0200

    new

--- src/java_rush/Ex1.java
+++ src/java_rush/Ex1.java
@@ -1,27 +1,27 @@
 package java_rush;
 
 public class Ex1 {
+    //создадим переменные вне класса и вызовем их
     //создать метод выводящий текст 1 раза
     //создать метод выводящий текст 3 раза
-    private String text1="mamka";
-    public String text2="papka";
-    private static String  text3="papik";
-    public static  String  text4="mamik";
-
+   private String name = "Den";
+   private static String secondName = "Papa";
+   public String name2 = "Vaska";
     public static void main(String[] args) {
-        Ex1 output=new Ex1();
+        Ex1 ex1=new Ex1();
         Ex1 out;
-        System.out.println((out=new Ex1()).text1); //инициализировали прямо в методе
-        System.out.println(output.text2);
-        System.out.println(text3);
-        System.out.println(text4);
-        multiPrint(text3,2);
-        System.out.println();
-        multiPrint(text4,2);
+        System.out.println((out=new Ex1()).name);
+        System.out.println(secondName);
+        System.out.println(ex1.name);
+        System.out.println(Ex1.secondName);
+        System.out.println(ex1.name2);
+        Ex1.multiprint(secondName,4);
+        multiprint(ex1.name,2);
+        multiprint(ex1.name2,9);
     }
-    public static void multiPrint(String s,int count){
-        for (int i=0;i<=count;i++){
-            System.out.print(s+" ");
+    public static void multiprint(String text,int count){
+        for (int i=0;i<count;i++){
+            System.out.print(text+" ");
         }
     }
 }
